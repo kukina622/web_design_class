2624342442d348173a34efd442b4010966465865
min <wyes91622@gmail.com>
2021-06-21 01:46:32 +0800

    first

---
+++ precache-manifest.9b6bc8f9fa29620a9615622d6dca0ef5.js
@@ -0,0 +1,350 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2bf62f9abc134737fc",
+    "url": "css/app.ffba112f.css"
+  },
+  {
+    "revision": "75118e553eb805db1bab",
+    "url": "css/chunk-vendors.3035f379.css"
+  },
+  {
+    "revision": "f8b1af7bad467753839902ac351f633d",
+    "url": "img/不動產事務科.f8b1af7b.jpg"
+  },
+  {
+    "revision": "fa81e2c8ab049edbf4f22c8779de2fc3",
+    "url": "img/冷凍空調科.fa81e2c8.jpg"
+  },
+  {
+    "revision": "520fb3f1e76b0b268afaff11e714c929",
+    "url": "img/化工科.520fb3f1.jpg"
+  },
+  {
+    "revision": "bd54e90424c80ae2bded63bf903feead",
+    "url": "img/商業經營科.bd54e904.jpg"
+  },
+  {
+    "revision": "28c00d1a7be7cb5300376bd0b8cca621",
+    "url": "img/商管財金科.28c00d1a.jpg"
+  },
+  {
+    "revision": "8438e2b96b1c1758319f6457b264b4a1",
+    "url": "img/國際貿易科.8438e2b9.jpg"
+  },
+  {
+    "revision": "996249d6db75db3c5a3edb306413ad20",
+    "url": "img/圖文傳播科.996249d6.jpg"
+  },
+  {
+    "revision": "5a451fe36a38e5a609df07484e3b516a",
+    "url": "img/土木建築科.5a451fe3.jpg"
+  },
+  {
+    "revision": "31e22165548508ec3a1bc35850f48aa1",
+    "url": "img/地球與環境學群.31e22165.jpg"
+  },
+  {
+    "revision": "787a5a3731aa52b607b61b260d346f9f",
+    "url": "img/外語學群.787a5a37.jpg"
+  },
+  {
+    "revision": "d1fef502c9588716e70a28e568832606",
+    "url": "img/外語科.d1fef502.jpg"
+  },
+  {
+    "revision": "ffacc1c62b6bc4461c64fa984c0dce42",
+    "url": "img/多媒體應用科.ffacc1c6.jpg"
+  },
+  {
+    "revision": "91819a56e8bd509c9d1cba942f49ecb7",
+    "url": "img/多媒體設計科.91819a56.jpg"
+  },
+  {
+    "revision": "254e369b6a80fdfaf86994659e43be5a",
+    "url": "img/大眾傳播學群.254e369b.jpg"
+  },
+  {
+    "revision": "3acab04c04624504b35f2ab840b6917c",
+    "url": "img/室內空間設計科.3acab04c.jpg"
+  },
+  {
+    "revision": "28edfb8b091f30545eea8ec1b60e26f2",
+    "url": "img/室內設計科.28edfb8b.jpg"
+  },
+  {
+    "revision": "9657e53e5f78652844b23835fb1c722b",
+    "url": "img/家具木工科.9657e53e.jpg"
+  },
+  {
+    "revision": "80b06ceae07e6e7208bb6d00032c4ff3",
+    "url": "img/家具設計科.80b06cea.jpg"
+  },
+  {
+    "revision": "da2f76c38bcde9f730ba8d3d2f7ad73e",
+    "url": "img/工業工程與管理科.da2f76c3.jpg"
+  },
+  {
+    "revision": "ac5b727016f0cdec263c284f4d03c236",
+    "url": "img/工程學群.ac5b7270.jpg"
+  },
+  {
+    "revision": "9a13586c3d6acd6c4d96087cced29d9f",
+    "url": "img/幼保科.9a13586c.jpg"
+  },
+  {
+    "revision": "23be215cbad57bbf221c2c4e0553b7cb",
+    "url": "img/廣告設計科.23be215c.jpg"
+  },
+  {
+    "revision": "af8324b20e9e2441602b288f0b605acc",
+    "url": "img/建築與設計學群.af8324b2.jpg"
+  },
+  {
+    "revision": "74d14a770611fdf126b07789fd1f5255",
+    "url": "img/控制科.74d14a77.jpg"
+  },
+  {
+    "revision": "c82a1244ffcffa7e7899905ae4589292",
+    "url": "img/數理化學群.c82a1244.jpg"
+  },
+  {
+    "revision": "d3434519760d58a06ced2f56ad372cb8",
+    "url": "img/文創設計科.d3434519.jpg"
+  },
+  {
+    "revision": "839dacd286bd0af2243cfbdd55dc0730",
+    "url": "img/文史哲學群.839dacd2.jpg"
+  },
+  {
+    "revision": "4fee75a00cea765a9818de8712001adb",
+    "url": "img/文書事務科.4fee75a0.jpg"
+  },
+  {
+    "revision": "2a4350b81ac323c0805d1850b1a77ba7",
+    "url": "img/會計事務科.2a4350b8.jpg"
+  },
+  {
+    "revision": "eafec12d76cb188aeeb40e660f7353bc",
+    "url": "img/板金科.eafec12d.jpg"
+  },
+  {
+    "revision": "14d5ba221b7aabeae6bb9200c064582d",
+    "url": "img/模具科.14d5ba22.jpg"
+  },
+  {
+    "revision": "c2a94814360039df7c819d2286f8fd3f",
+    "url": "img/機械木模科.c2a94814.jpg"
+  },
+  {
+    "revision": "7c30fa5518e2015357c4cd39b881394d",
+    "url": "img/機械科.7c30fa55.jpg"
+  },
+  {
+    "revision": "09f6bdabe1806b14086319283aa21fbe",
+    "url": "img/機電科.09f6bdab.jpg"
+  },
+  {
+    "revision": "c19c0b26d007a2a95580ecd755c069cd",
+    "url": "img/水產經營科.c19c0b26.jpg"
+  },
+  {
+    "revision": "fbea884ea787f947ed73050b40dcacec",
+    "url": "img/法政學群.fbea884e.jpg"
+  },
+  {
+    "revision": "1bdde213a54ae430f62572ff175294ae",
+    "url": "img/流通管理科.1bdde213.png"
+  },
+  {
+    "revision": "c6cb5c365ce9ac37e37c3aa97d3a02aa",
+    "url": "img/海事科.c6cb5c36.jpg"
+  },
+  {
+    "revision": "e2ec760207d3b14a2baab2646d781c15",
+    "url": "img/生命科學學群.e2ec7602.jpg"
+  },
+  {
+    "revision": "3d19d5e83a07a59430ff5526ae797435",
+    "url": "img/生命關懷事業科.3d19d5e8.jpg"
+  },
+  {
+    "revision": "7e8ab4cd12b25f27def167443f2c7dc8",
+    "url": "img/生物產業機電科.7e8ab4cd.jpg"
+  },
+  {
+    "revision": "85d66e6734e9a7a7ee5962f3abeba49a",
+    "url": "img/生物資源學群.85d66e67.jpg"
+  },
+  {
+    "revision": "6f8c0cea0f85467ebeb2d675faef2bff",
+    "url": "img/社會與心理學群(心理).6f8c0cea.jpg"
+  },
+  {
+    "revision": "ac26afdca918b085b40272bb34739faa",
+    "url": "img/社會與心理學群(社會).ac26afdc.jpg"
+  },
+  {
+    "revision": "e182e120b70c5b2a4b9fbbbbc5d407d0",
+    "url": "img/管理學系.e182e120.jpg"
+  },
+  {
+    "revision": "49ff4aef353ea852a59a8c02421f3ef5",
+    "url": "img/管理學群.49ff4aef.jpg"
+  },
+  {
+    "revision": "ad642b073aa74855117279a2381a8a35",
+    "url": "img/美容科.ad642b07.jpg"
+  },
+  {
+    "revision": "2455f81bd5a52e96fc63100961648830",
+    "url": "img/美工科.2455f81b.jpg"
+  },
+  {
+    "revision": "3407d526fbfdf144b3cf409e9451002c",
+    "url": "img/老人照顧科.3407d526.jpg"
+  },
+  {
+    "revision": "3fb1b35e98062ecc17afccd4fdb7e2c0",
+    "url": "img/航空電子科.3fb1b35e.jpg"
+  },
+  {
+    "revision": "5c56afa9bbb23d4108c829457ae2791d",
+    "url": "img/航運管理科.5c56afa9.jpg"
+  },
+  {
+    "revision": "cf07d070f1b277e54a9b56e14761d2b4",
+    "url": "img/藝術學群.cf07d070.jpg"
+  },
+  {
+    "revision": "7a9ea72ec96b6a89d264bab88c9b5750",
+    "url": "img/藝術與表演科.7a9ea72e.jpg"
+  },
+  {
+    "revision": "c9ebfb374a352dd501641ae0f89cd893",
+    "url": "img/衛生科.c9ebfb37.jpg"
+  },
+  {
+    "revision": "f30911f01b420b5331aca052f0d7687c",
+    "url": "img/製圖科.f30911f0.jpg"
+  },
+  {
+    "revision": "d32da943b9940f925b9989beede195eb",
+    "url": "img/視光科.d32da943.gif"
+  },
+  {
+    "revision": "c103a0e0915db64906b7f06c70bd0982",
+    "url": "img/觀光事業科.c103a0e0.jpg"
+  },
+  {
+    "revision": "6bd75321d33f4d78cbdbeb90fc58e6e6",
+    "url": "img/觀光休閒科.6bd75321.jpg"
+  },
+  {
+    "revision": "5d086e015d2e1df9ee6bac5454309920",
+    "url": "img/護理科.5d086e01.jpg"
+  },
+  {
+    "revision": "f2e3ec15b4fff78864f3c7f74e3486c7",
+    "url": "img/財經學群.f2e3ec15.jpg"
+  },
+  {
+    "revision": "ae281e6473ded1c5dfc2f5a67392136f",
+    "url": "img/資料處理科.ae281e64.jpg"
+  },
+  {
+    "revision": "0954f12a8fac4062c8bcde24d78e8d4f",
+    "url": "img/資訊學群.0954f12a.jpg"
+  },
+  {
+    "revision": "6d1c592d3546974a0b01e86b09a90d5a",
+    "url": "img/資訊科.6d1c592d.jpg"
+  },
+  {
+    "revision": "b2b4cdb1f847081cae4218d5b14a41a5",
+    "url": "img/資訊管理科.b2b4cdb1.jpg"
+  },
+  {
+    "revision": "022a1965d381cbad6f7b2ad7b9131efa",
+    "url": "img/農業科.022a1965.jpg"
+  },
+  {
+    "revision": "fcfac4609818122ff92d1578920ccdde",
+    "url": "img/農產行銷科.fcfac460.jpg"
+  },
+  {
+    "revision": "62e17a14f7808ccd6e5585b2d7aa21d2",
+    "url": "img/配管科.62e17a14.jpg"
+  },
+  {
+    "revision": "c7e53b0ce99b0be1140429744f6b4502",
+    "url": "img/醫藥衛生學群.c7e53b0c.jpg"
+  },
+  {
+    "revision": "f683bf81057e0b21ff4adf881c86fc6d",
+    "url": "img/金屬工藝科.f683bf81.jpg"
+  },
+  {
+    "revision": "5980ca2c36716748444aff37aca7dfe8",
+    "url": "img/鑄造科.5980ca2c.jpg"
+  },
+  {
+    "revision": "318f64133a623cb6909a96a0ae9e4e21",
+    "url": "img/陶瓷工程科.318f6413.jpg"
+  },
+  {
+    "revision": "b99211bfe6b15df1221414d42f7081d9",
+    "url": "img/電子商務科.b99211bf.jpg"
+  },
+  {
+    "revision": "ed922f6199f420701d74097dfbb816f0",
+    "url": "img/電子科.ed922f61.jpg"
+  },
+  {
+    "revision": "7550675099d30524fd325c7411d05de4",
+    "url": "img/電子通信科.75506750.jpg"
+  },
+  {
+    "revision": "957cca5801c5c91d6d29cc8e0b87508b",
+    "url": "img/電機科.957cca58.jpg"
+  },
+  {
+    "revision": "eff71d76044c2d6e13c4498d6bcaa0b0",
+    "url": "img/電機電子科.eff71d76.jpg"
+  },
+  {
+    "revision": "ceccce9b371df30251595eccaf58dfb9",
+    "url": "img/電腦機械製圖科.ceccce9b.jpg"
+  },
+  {
+    "revision": "cfa3fcbfd155d6d669c5ccae2100ac5b",
+    "url": "img/食品科.cfa3fcbf.jpg"
+  },
+  {
+    "revision": "94a6d3800a0abb2a311251f80f673ea5",
+    "url": "img/餐旅管理科.94a6d380.jpg"
+  },
+  {
+    "revision": "c1489ff8895d961f05e5310642962288",
+    "url": "img/餐飲管理科.c1489ff8.jpg"
+  },
+  {
+    "revision": "0096bd6727ce9626a410a16e4525e69f",
+    "url": "index.html"
+  },
+  {
+    "revision": "de2bf62f9abc134737fc",
+    "url": "js/app.3c334923.js"
+  },
+  {
+    "revision": "75118e553eb805db1bab",
+    "url": "js/chunk-vendors.83657ec5.js"
+  },
+  {
+    "revision": "3ed5440ee4fb5cc12cbdf695fd4a46cb",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
